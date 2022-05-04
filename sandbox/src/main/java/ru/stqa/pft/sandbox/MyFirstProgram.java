1d3f51ddd41e6ba9f14528fe6c78a1e6a7edaea5
TyurikovaEvgeniya <kirra.sekirra@gmail.com>
2022-05-04 20:33:19 +0300

    Добавлены классы, их конструкторы и методы ассоциированные с классами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,13 +6,12 @@ public class MyFirstProgram {
     hello("user");
     hello("Eugeniya");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
 
-    System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь треугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
@@ -20,11 +19,4 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
 }
