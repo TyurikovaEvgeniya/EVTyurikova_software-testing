c82994ead5deba65e041beb459094f621a60794c
TyurikovaEvgeniya <kirra.sekirra@gmail.com>
2022-05-06 12:30:54 +0300

    Задание №2. Добавлен метод distanceToPoint в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -12,7 +12,7 @@ public class Point {
     this.y = y;
   }
 
-  public static double distance(Point p1, Point p2) {
-    return sqrt(pow((p2.x - p1.x),2) + pow((p2.y - p1.y),2));
+  public double distanceToPoint(Point TargetPoint) {
+    return sqrt(pow((TargetPoint.x - this.x),2) + pow((TargetPoint.y - this.y),2));
   }
 }
