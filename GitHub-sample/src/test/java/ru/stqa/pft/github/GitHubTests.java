7d008b6669a232860c3f7693af06da2de15eccb1
TyurikovaEvgeniya <kirra.sekirra@gmail.com>
2022-09-02 17:30:11 +0300

    Лекция 9.6. REST: взаимодействие с GitHub

--- GitHub-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
+++ GitHub-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -4,15 +4,17 @@ import com.google.common.collect.ImmutableMap;
 import com.jcabi.github.*;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class GitHubTests {
 
 
   @Test
-  public void testCommits() {
-    Github github = new RtGithub("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");
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
     RepoCommits commits = github.repos().get(new Coordinates.Simple("TyurikovaEvgeniya", "EVTyurikova_software-testing")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
-      System.out.println(commit);
+      System.out.println(new RepoCommit.Smart(commit).message());
     }
   }
 }
